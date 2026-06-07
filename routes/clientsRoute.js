f5a043a4bb0256794de706db6ed9e6e6cc33ce9f
deibido0604 <davidjared1104@gmail.com>
2026-06-07 11:52:42 -0600

    Change clientes endpoint =

--- routes/clientsRoute.js
+++ routes/clientsRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const clientsController = require('../controllers/clientsController');
 
-router.get('/', clientsController.getAll);
+router.get('/list', clientsController.getAll);
 router.get('/:id', clientsController.getById);
 router.post('/', clientsController.create);
 router.put('/:id', clientsController.update);
